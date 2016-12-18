285b5eaea1a33fd2c7c8eb640fea394aaf21888d
Petteri Vaarala <petteri.vaarala@gmail.com>
2016-12-18 02:54:24 +0200

    Added gitconfig & gitignore_global

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 # bootstrap.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 
-files="vimrc vim bash_aliases"    # list of files/folders to symlink in homedir
+# list of files/folders to symlink in homedir
+files="vimrc vim bash_aliases gitconfig gitignore_global"
 
 for file in $files; do
     if [ -f ~/.$file ]; then
